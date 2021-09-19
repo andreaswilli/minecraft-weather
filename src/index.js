f473de8832dcb5aa93e3b5a7c2639592cefb03c9
Andreas Willi <andiwilli8460@gmail.com>
2021-09-19 11:08:18 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,124 @@
+const isFahrenheit = /[&?]unit=([^&]+)/.exec(location.search)?.[1] === "f";
+
+(async function () {
+  try {
+    setText(".description", "loading...");
+    const current_weather = await getCurrentWeather();
+    if (isFahrenheit) {
+      setText(".temperature", current_weather.temp_F + "°F");
+    } else {
+      setText(".temperature", current_weather.temp_C + "°C");
+    }
+    setText(".description", current_weather.weatherDesc[0].value);
+    const weather_description =
+      WEATHER_MAPPING[WWO_CODE[current_weather.weatherCode]];
+    setImage(weather_description);
+  } catch (e) {
+    setText(
+      ".description",
+      "API request failed. Was it blocked by your ad/script blocker?"
+    );
+  }
+})();
+
+async function getCurrentWeather() {
+  const ip_res = await fetch("https://api.ipify.org");
+  const ip = await ip_res.text();
+  const weather_res = await fetch(`https://wttr.in/${ip}?format=j1`);
+  const weather = await weather_res.json();
+  return weather.current_condition[0];
+}
+
+function setText(selector, text) {
+  document.querySelector(selector).innerText = text;
+}
+
+function setImage(description) {
+  const max = NO_OF_IMAGES[description];
+  const random = Math.floor(Math.random() * max + 1);
+  document
+    .querySelector(".bg-image")
+    .setAttribute("src", `/assets/images/${description}_${random}.png`);
+}
+
+// https://github.com/chubin/wttr.in/blob/master/lib/constants.py
+WWO_CODE = {
+  113: "Sunny",
+  116: "PartlyCloudy",
+  119: "Cloudy",
+  122: "VeryCloudy",
+  143: "Fog",
+  176: "LightShowers",
+  179: "LightSleetShowers",
+  182: "LightSleet",
+  185: "LightSleet",
+  200: "ThunderyShowers",
+  227: "LightSnow",
+  230: "HeavySnow",
+  248: "Fog",
+  260: "Fog",
+  263: "LightShowers",
+  266: "LightRain",
+  281: "LightSleet",
+  284: "LightSleet",
+  293: "LightRain",
+  296: "LightRain",
+  299: "HeavyShowers",
+  302: "HeavyRain",
+  305: "HeavyShowers",
+  308: "HeavyRain",
+  311: "LightSleet",
+  314: "LightSleet",
+  317: "LightSleet",
+  320: "LightSnow",
+  323: "LightSnowShowers",
+  326: "LightSnowShowers",
+  329: "HeavySnow",
+  332: "HeavySnow",
+  335: "HeavySnowShowers",
+  338: "HeavySnow",
+  350: "LightSleet",
+  353: "LightShowers",
+  356: "HeavyShowers",
+  359: "HeavyRain",
+  362: "LightSleetShowers",
+  365: "LightSleetShowers",
+  368: "LightSnowShowers",
+  371: "HeavySnowShowers",
+  374: "LightSleetShowers",
+  377: "LightSleet",
+  386: "ThunderyShowers",
+  389: "ThunderyHeavyRain",
+  392: "ThunderySnowShowers",
+  395: "HeavySnowShowers",
+};
+
+WEATHER_MAPPING = {
+  Cloudy: "cloudy",
+  Fog: "fog",
+  HeavyRain: "rain",
+  HeavyShowers: "rain",
+  HeavySnow: "snow",
+  HeavySnowShowers: "snow",
+  LightRain: "rain",
+  LightShowers: "rain",
+  LightSleet: "snow",
+  LightSleetShowers: "snow",
+  LightSnow: "snow",
+  LightSnowShowers: "snow",
+  PartlyCloudy: "cloudy",
+  Sunny: "sunny",
+  ThunderyHeavyRain: "thundery",
+  ThunderyShowers: "thundery",
+  ThunderySnowShowers: "thundery",
+  VeryCloudy: "cloudy",
+};
+
+const NO_OF_IMAGES = {
+  sunny: 5,
+  cloudy: 5,
+  fog: 5,
+  rain: 5,
+  snow: 6,
+  thundery: 3,
+};
